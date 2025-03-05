a53be97b540df741146c8c9ea0ec6d76df3a5583
SandhanuDulmeth <sandhanu12@gmail.com>
2025-03-06 00:17:59 +0530

    add login and customer components with basic structure and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './common/nav-bar/nav-bar.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NavBarComponent],
+  imports: [RouterOutlet,NavBarComponent,LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ItemsComponent } from './pages/items/items.component';
+import { CustomerComponent } from './pages/customer/customer.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [{
     path: 'home',
@@ -11,6 +13,14 @@ export const routes: Routes = [{
     path: 'items',
     component: ItemsComponent
 
+},
+{
+    path:'customer',
+    component:CustomerComponent
+},
+{
+    path:'login',
+    component:LoginComponent
 }
 
 
