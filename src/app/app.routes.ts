10b4d98259e4c7dfca2c62da4bd1440370138cad
SandhanuDulmeth <sandhanu12@gmail.com>
2025-03-05 23:35:04 +0530

    add items component, nav-bar updates, and integrate Bootstrap styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ItemsComponent } from './pages/items/items.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: 'home',
+    component: HomeComponent
+
+},
+{
+    path: 'items',
+    component: ItemsComponent
+
+}
+
+
+];
