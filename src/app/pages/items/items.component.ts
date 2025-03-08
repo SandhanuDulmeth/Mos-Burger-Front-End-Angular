13d8fc64f3fefed3b25876cf0933dd4aef644e48
SandhanuDulmeth <sandhanu12@gmail.com>
2025-03-08 09:54:20 +0530

    add menu component with item filtering and cart functionality

--- src/app/pages/items/items.component.ts
+++ src/app/pages/items/items.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+interface MenuItem {
+  id: number;
+  name: string;
+  price: number;
+  category: string;
+  image: string;
+}
 
 @Component({
   selector: 'app-items',
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './items.component.html',
   styleUrl: './items.component.css'
 })
-export class ItemsComponent {
+export class ItemsComponent implements OnInit {
+
+public categories: any ;
+  constructor(){
+    this.categories =['All', 'Burger', 'Submarine', 'Fries', 'Pasta', 'Chicken', 'Beverage'];
+  }
+
+   menuItems: MenuItem[] = [];
+
+  ngOnInit(): void {
+     }
+
+  filterItems(cat: any) {
+    console.log(cat);
+    
+  }
+  addToCart(item:any){
+    console.log(item);
+    
+  }
+
 
 }
