d3fd608bb5f016bcb48c47e1df9523d20fd10a9b
SandhanuDulmeth <sandhanu12@gmail.com>
2025-03-11 09:38:17 +0530

    refactor: remove redundant comments in customer component for improved clarity

--- src/app/pages/customer/customer.component.ts
+++ src/app/pages/customer/customer.component.ts
@@ -15,18 +15,18 @@ interface Customer {
 })
 export class CustomerComponent implements OnInit {
   customers: Customer[] = [];
-  // This object holds the form data
+
   customerForm: Customer = { name: '', email: '', phone: '', address: '' };
-  // Track whether we're editing an existing customer (by index) or adding a new one
+
   editingIndex: number = -1;
-  // Model for search input
+
   searchInput: string = '';
 
   ngOnInit(): void {
     this.loadCustomersFromLocalStorage();
   }
 
-  // Called when the form is submitted
+
   onSubmit(): void {
     if (!this.customerForm.name || !this.customerForm.email || !this.customerForm.phone || !this.customerForm.address) {
       alert('Please fill in all fields');
@@ -39,7 +39,7 @@ export class CustomerComponent implements OnInit {
       this.updateCustomer(this.editingIndex, { ...this.customerForm });
       this.editingIndex = -1;
     }
-    // Reset the form
+    
     this.customerForm = { name: '', email: '', phone: '', address: '' };
   }
 
@@ -75,7 +75,7 @@ export class CustomerComponent implements OnInit {
     }
   }
 
-  // A computed property to filter customers based on the search input
+
   get filteredCustomers(): Customer[] {
     if (!this.searchInput) {
       return this.customers;
